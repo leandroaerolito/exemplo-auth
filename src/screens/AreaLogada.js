c22a2fc9ff18b3150b1e4c9b6953337cd65882dd
Leandro Rocha <leandro.aerolito@gmail.com>
2024-03-19 09:33:17 -0300

    Navegação na "Área Logada"

--- src/screens/AreaLogada.js
+++ src/screens/AreaLogada.js
@@ -2,20 +2,28 @@ import { Button, StyleSheet, Text, View } from "react-native";
 
 import { auth } from "../../firebase.config";
 
-export default function AreaLogada() {
+export default function AreaLogada(navigation) {
   // Acessando dados do usuário logado
   // console.log(auth.currentUser);
 
   const { email } = auth.currentUser;
   console.log(email);
 
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      navigation.replace("Inicial");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <View style={estilos.container}>
       <View style={estilos.topo}>
         <Text style={estilos.bemVindo}>Bem-vindo(a)</Text>
         <Text>Seu e-mail de cadastro é:</Text>
         <Text style={{ fontWeight: "bold" }}>{email}</Text>
-        <Button title="Logout" color="#D35400" />
+        <Button onPress={logout} title="Logout" color="#D35400" />
       </View>
       <View style={estilos.geral}>
         <Text>Você está na área logada.</Text>
